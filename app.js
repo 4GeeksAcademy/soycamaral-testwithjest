928afbbb9ec16a4020aeccc6f81127f59092748b
soycamaral <camaralr95@gmail.com>
2024-10-20 01:11:03 +0000

    First commit

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
+const euroToDollar = 1.07; 
+const dollarToYen = 146.26; 
+
 const fromEuroToDollar = function(valueInEuro) {
-    let valueInDollar = valueInEuro * 1.07;
+    let valueInDollar = valueInEuro * euroToDollar; 
     return valueInDollar;
 }
 
-const fromDollarToYen = function(valueinDollar) {
-    let valueinYen = valueinDollar * 146.26;
-    return valueinYen;
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * dollarToYen; 
+    return valueInYen;
 }
 
-const fromYenToPound = function (valueinYen) {
-    let valueinPound = valueinYen * 0.87
-    return valueinPound;
-
+const fromYenToPound = function(valueInYen) {
+    let valueInEuro = valueInYen / 156.5;
+    let valueInPound = valueInEuro * 0.87;
+    return valueInPound;
 }
 
 const sum = (a,b) => {
-    return a + b
+    return a + b;
 }
-console.log(sum(7,3))
 
+console.log(sum(7,3));
 
-module.exports={ sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
+module.exports={ sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
